d764e334a2a07020ba7fd05ac01a1b87e85bae70
Bosh-Kuo <evaktm1680@gmail.com>
2021-07-21 16:44:26 +0800

    update to Ex2_2

---
+++ Section3_Linear_Algebra_with_Numpy/Ex2_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+ 
+A = np.array([[1,2,3],[2,1,-1]])
+B = np.array([[1,-2,0],[3,0,1],[4,5,6]])
+C = np.array(range(1,10)).reshape(3,3)
+D = np.array([[1],[4],[-3]])
+print(A)
+print('\n',B)
+print('\n',C)
+print('\n',D)
---
+++ Section3_Linear_Algebra_with_Numpy/Ex2_2.py
@@ -0,0 +1,9 @@
+import numpy as np
+ 
+E = np.array(range(1,7)).reshape(6,1)
+F = np.array([1,0,3,-10])
+G = np.array(range(1,6))
+
+print(E)
+print('\n',F)
+print('\n',G)
